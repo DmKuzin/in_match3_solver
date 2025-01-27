45aa9a18b4176b676c856f166472675c787383b1
dmitrii.kuzin <dmitrii.kuzin@axenix.pro>
2025-01-27 17:10:14 +0300

    add matrix confidence slider

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ if source_radio == settings.IMAGE:
 
     # Контейнер для первой колонки с исходным изображением
     with col1:
-        with st.container(height=settings.CONTAINER_HEIGHT):  # Высота в пикселях
+        #with st.container(height=settings.CONTAINER_HEIGHT):  # Высота в пикселях
+        with st.container(height=None):  # Высота в пикселях
             st.write("Source image")
             try:
                 # Если изображение загружено
@@ -95,7 +96,8 @@ if source_radio == settings.IMAGE:
 
             # Контейнер для второй колонки с результатом детекции
             with col2:
-                with st.container(height=settings.CONTAINER_HEIGHT):
+                #with st.container(height=settings.CONTAINER_HEIGHT):
+                with st.container(height=None):  # Высота в пикселях
                     st.write("Detected image")
                     st.image(res_plotted, caption='Detected Image', width=settings.IMAGE_WIDTH)  # Ограничиваем ширину
 
@@ -108,7 +110,8 @@ if source_radio == settings.IMAGE:
 
             # Контейнер для третьей колонки с результатом детекции
             with col3:
-                with st.container(height=settings.CONTAINER_HEIGHT):
+                #with st.container(height=settings.CONTAINER_HEIGHT):
+                with st.container(height=None):  # Высота в пикселях
                     st.write("Result matrix")
                     # Вызываем функцию detect_images_in_grid для создания матрицы
                     grid_board = make_matrix.detect_images_in_grid(res, confidence_threshold=matrix_confidence)
